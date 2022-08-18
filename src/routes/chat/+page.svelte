5df642a5eccc7467dbc5a59eacd2d6bf1c128fe6
magnetenstad <tenstad.magne@gmail.com>
2022-08-18 23:16:20 +0200

    Update Svelte

---
+++ src/routes/chat/+page.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import Chat from '$lib/components/Chat.svelte';
+  import { Message } from '$lib/Message';
+  let messages: Array<Message> = [
+    new Message(true, 'hey there!'),
+    new Message(true, 'hey there!'),
+    new Message(true, 'hey there!'),
+    new Message(true, 'hey there!'),
+    new Message(true, 'hey there!'),
+    new Message(true, 'hey there!'),
+    new Message(true, 'hey there!'),
+    new Message(false, 'hey there!'),
+    new Message(false, 'hey there!'),
+    new Message(false, 'hey there!'),
+    new Message(true, 'hey there!')
+  ];
+</script>
+
+<div class="wrapper">
+  <div class="body">
+    <Chat imgSrc="https://avatars.githubusercontent.com/u/46494695" {messages} />
+  </div>
+</div>
+
+<style>
+  .wrapper {
+    background-color: var(--color-background);
+    min-height: 100vh;
+    overflow: hidden;
+  }
+
+  .body {
+    display: flex;
+    flex-direction: row;
+    align-items: stretch;
+    height: 100vh;
+  }
+</style>
