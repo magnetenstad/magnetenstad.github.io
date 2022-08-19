bd67dd0075138d5c243c42d418f650b5fc2d2ce7
magnetenstad <tenstad.magne@gmail.com>
2022-08-19 02:18:50 +0200

    Improve HelloDialog scaling

--- src/lib/components/HelloDialog.svelte
+++ src/lib/components/HelloDialog.svelte
@@ -2,25 +2,23 @@
 </script>
 
 <div class="dialog">
-	<div class="body">
-		<slot />
-	</div>
+  <div class="body">
+    <slot />
+  </div>
 </div>
 
 <style>
-	.dialog {
-		position: fixed;
-		left: 50%;
-		top: 50%;
-		transform: translate(-50%, -50%);
-		max-width: 24em;
-		padding: 2em;
-		background-color: white;
-		border-radius: 1em;
-		box-shadow: 0 1em 2em var(--color-shadow);
-	}
+  .dialog {
+    padding: 2em;
 
-	.body {
-		max-width: 75%;
-	}
+    position: fixed;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    width: min(24em, 80vw);
+
+    background-color: white;
+    border-radius: 1em;
+    box-shadow: 0 1em 2em var(--color-shadow);
+  }
 </style>
