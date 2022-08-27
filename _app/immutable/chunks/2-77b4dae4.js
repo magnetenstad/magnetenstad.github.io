89933dd1a17db91bea7093c831e838c5dad5d082
magnetenstad <46494695+magnetenstad@users.noreply.github.com>
2022-08-27 21:58:04 +0000

    Deploying to gh-pages from @ magnetenstad/magnetenstad.github.io@86291a24e3aefb7c440b3889fe3f7e490c16c5fa 🚀

---
+++ _app/immutable/chunks/2-77b4dae4.js
@@ -0,0 +1,1 @@
+import{default as e}from"../components/pages/_page.svelte-e13d4c7d.js";import"./index-1d50e03e.js";import"./Button-21dca69e.js";import"./singletons-e5e57fbd.js";export{e as component};
