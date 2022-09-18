6e45e6794ceb69d49f6eb48508454f6a529e1668
magnetenstad <tenstad.magne@gmail.com>
2022-09-18 16:00:16 +0200

    Update svelte-markdown

--- svelte.config.js
+++ svelte.config.js
@@ -11,10 +11,7 @@ const config = {
       assets: 'build',
       fallback: null
     }),
-    trailingSlash: 'always',
-    prerender: {
-      default: true
-    }
+    trailingSlash: 'always'
   }
 };
 
