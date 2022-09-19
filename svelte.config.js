407979667bf4d16791089be5230d049332d1039c
magnetenstad <tenstad.magne@gmail.com>
2022-09-20 01:50:54 +0200

    Update adapter-static

--- svelte.config.js
+++ svelte.config.js
@@ -2,17 +2,18 @@ import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
   preprocess: preprocess(),
   kit: {
     // https://www.npmjs.com/package/@sveltejs/adapter-static
     adapter: adapter({
+      // default options are shown. On some platforms
+      // these options are set automatically — see below
       pages: 'build',
       assets: 'build',
-      fallback: null
+      fallback: null,
+      precompress: false
     }),
     trailingSlash: 'always'
   }
 };
-
-export default config;
